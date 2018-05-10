d7adc24d82577335e7839519476a505a8ce99398
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-10 12:20:47 +0200

    dependencies and readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>,
+    document.getElementById('root'));
