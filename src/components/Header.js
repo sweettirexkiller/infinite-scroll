6575cebde84f3b415d118632528d63ef9512e4ab
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-10 17:34:52 +0200

    a little change in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default () => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <Link to="/" className="brand-logo">Dogs</Link>
+                <Link to="/" className="brand-logo">Click on image to see profile</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                 </ul>
             </div>
